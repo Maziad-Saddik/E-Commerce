bac1eb4e391606f261091f201417838bbfa16f8f
saddik-maziad <saddikmaziad@gmail.com>
2025-06-03 13:53:25 +0300

    Add Refernce project

---
+++ src/commands/E-Commerce.Infrastructure.Commands/Entities/OutboxMessageEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace E_Commerce.Infrastructure.Entities;
+
+public class OutboxMessageEntity
+{
+    public OutboxMessageEntity()
+    { }
+    public OutboxMessageEntity(Event @event)
+    {
+        Event = @event;
+    }
+
+    public int Id { get; set; }
+    public Event? Event { get; set; }
+}
