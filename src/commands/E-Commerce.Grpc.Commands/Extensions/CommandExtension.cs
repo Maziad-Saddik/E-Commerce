d77b36cb89067a15b44ec11af975535f5c7c90ce
saddik-maziad <saddikmaziad@gmail.com>
2025-06-04 15:34:33 +0300

    Update Command Grpc and Test

--- src/commands/E-Commerce.Grpc.Commands/Extensions/CommandExtension.cs
+++ src/commands/E-Commerce.Grpc.Commands/Extensions/CommandExtension.cs
@@ -1,6 +1,30 @@
-﻿namespace Extensions;
+﻿using E_Commerce.Domain.Commands;
+using E_Commerce.Domain.ValueObjects;
+using E_Commerce.Grpc;
+
+namespace Extensions;
 
 public static class CommandExtension
 {
+    public static PlaceOrderCommand ToCommand(this PlaceOrderRequest request) => new()
+    {
+        OrderId = request.OrderId,
+        UserId = request.UserId,
+        Customer = E_Commerce.Domain.Entities.Customer.Add(
+           id: Guid.Parse(request.Customer.Id),
+           name: request.Customer.Name,
+           email: new Email(request.Customer.Email)
+        ),
+        orderItems = request.OrderItems.Select(x => E_Commerce.Domain.Entities.OrderItem.Add(
+           productRef: x.ProductRefenence,
+           quantity: x.Quantity,
+           price: new Money((decimal)x.Price, x.Currency))
+        ).ToList(),
+    };
 
+    public static CancelOrderCommand ToCommand(this CancelOrderRequest request) => new()
+    {
+        OrderId = request.OrderId,
+        UserId = request.UserId
+    };
 }
