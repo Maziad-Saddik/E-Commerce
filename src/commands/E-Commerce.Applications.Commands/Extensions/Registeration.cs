54b960a157db90a6b006f6c086059937298c7c7a
saddik-maziad <saddikmaziad@gmail.com>
2025-06-04 14:36:55 +0300

    Update Registeration Application and Program

---
+++ src/commands/E-Commerce.Applications.Commands/Extensions/Registeration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace E_Commerce.Applications.Extensions;
+
+public static class RegisterationExtensions
+{
+    public static void MediatrRegister(this IServiceCollection services)
+    {
+        //services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<>());
+    }
+}
+
--- src/commands/E-Commerce.Grpc.Commands/Program.cs
+++ src/commands/E-Commerce.Grpc.Commands/Program.cs
@@ -1,14 +1,31 @@
+using E_Commerce.Applications.Extensions;
+using E_Commerce.Infrastructure.Extensions;
+using E_Commerce.Infrastructure.Services.Logger;
+using Middleware;
+using Serilog;
 using Services;
+
+Log.Logger = LoggerServiceBuilder.Build();
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
 
+builder.Services.InfrastructureRegister(builder.Configuration);
+
+builder.Services.MediatrRegister();
+
+//builder.Services.AddGrpcWithValidators();
+
+builder.Host.UseSerilog();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseMiddleware<CultureInfoManager>();
+
 app.MapGrpcService<OrdersCommandService>();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
