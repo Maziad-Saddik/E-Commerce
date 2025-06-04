3ca9e1f084ad1ebc27b8c50be75cb688e374fdc5
saddik-maziad <saddikmaziad@gmail.com>
2025-06-04 13:03:48 +0300

    Added Commands

---
+++ src/commands/E-Commerce.Domain.Commands/Commands/CancelOrderCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace E_Commerce.Domain.Commands
+{
+    public class CancelOrderCommand : IRequest<string>
+    {
+        public required string OrderId { get; init; }
+
+        public required string UserId { get; init; }
+    }
+}
---
+++ src/commands/E-Commerce.Domain.Commands/Commands/PlaceOrderCommand.cs
@@ -0,0 +1,14 @@
+﻿using E_Commerce.Domain.Entities;
+using MediatR;
+
+namespace E_Commerce.Domain.Commands
+{
+    public class PlaceOrderCommand : IRequest<string>
+    {
+        public required string OrderId { get; init; }
+
+        public required string UserId { get; init; }
+
+        public required Customer Customer { get; init; }
+    }
+}
