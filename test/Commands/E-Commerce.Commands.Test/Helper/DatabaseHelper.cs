9b96153605fd97aaa2dd1f07ddf1bd9feda6c11b
saddik-maziad <saddikmaziad@gmail.com>
2025-06-04 14:28:33 +0300

    Update DataBaseHelper and PalceOrderTest

--- test/Commands/E-Commerce.Commands.Test/Helper/DatabaseHelper.cs
+++ test/Commands/E-Commerce.Commands.Test/Helper/DatabaseHelper.cs
@@ -1,9 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using E_Commerce.Applications.Contracts;
+using E_Commerce.Domain.Events;
+using E_Commerce.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace E_Commerce.Commands.Test.Helper
 {
     public class DatabaseHelper(WebApplicationFactory<Program> factory)
     {
+        public async Task<List<Event>> GetAllAsync(string aggregateId)
+        {
+            using IServiceScope scope = factory.Services.CreateScope();
 
+            IEventStore eventStore = scope.ServiceProvider.GetRequiredService<IEventStore>();
+
+            return await eventStore.GetAllAsync(aggregateId, default);
+        }
+
+        public async Task AddEventAsync(Event @event)
+        {
+            using IServiceScope scope = factory.Services.CreateScope();
+
+            IEventStore eventStore = scope.ServiceProvider.GetRequiredService<IEventStore>();
+
+            await eventStore.CommitAsync(@event, default);
+        }
+
+        public async Task AddEventsAsync(IReadOnlyList<Event> events)
+        {
+            using IServiceScope scope = factory.Services.CreateScope();
+
+            IEventStore eventStore = scope.ServiceProvider.GetRequiredService<IEventStore>();
+
+            await eventStore.CommitAsync(events, default);
+        }
+
+        public async Task<List<Event>> GetEventsBySequenceAsync(string aggregateId, int minSequence = 0)
+        {
+            using IServiceScope scope = factory.Services.CreateScope();
+
+            AppDbContext dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            return await dbContext.Events
+                .Where(x => x.Sequence >= minSequence && x.AggregateId.Equals(aggregateId))
+                .OrderBy(x => x.Id)
+                .ToListAsync();
+        }
+
+        public async Task DeleteEventsAsync()
+        {
+            using IServiceScope scope = factory.Services.CreateScope();
+
+            AppDbContext dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            await dbContext.Events.ExecuteDeleteAsync();
+        }
     }
 }
---
+++ test/Commands/E-Commerce.Commands.Test/Tests/PlaceOrderTest.cs
@@ -0,0 +1,60 @@
+﻿using Calzolari.Grpc.Net.Client.Validation;
+using E_Commerce.Commands.Test.Helper;
+using E_Commerce.Infrastructure.Persistence;
+using Grpc.Core;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit.Abstractions;
+
+namespace E_Commerce.Commands.Test.Tests
+{
+    public class PlaceOrderTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly DatabaseHelper _databaseHelper;
+
+        private readonly GrpcHelper _grpcHelper;
+
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public PlaceOrderTest(WebApplicationFactory<Program> factory, ITestOutputHelper helper)
+        {
+            _factory = factory.WithDefaultConfigurations(helper, services =>
+            {
+                services.SetUnitTestsIsolatedEnvironment();
+            });
+
+            _databaseHelper = new DatabaseHelper(_factory);
+
+            _grpcHelper = new GrpcHelper(_factory);
+        }
+
+        [Fact]
+        public async Task VerifyDatabaseConnection()
+        {
+            using var scope = _factory.Services.CreateScope();
+
+            var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            var canConnect = await dbContext.Database.CanConnectAsync();
+
+            Assert.True(canConnect, "Database connection failed.");
+        }
+
+        [Theory]
+        [InlineData("")]
+        public async Task PlaceOrder_InvalidData_ThrowsInvalidArgument(
+           string errorPropertyName
+        )
+        {
+            RpcException exception = await Assert.ThrowsAsync<RpcException>
+               (() => _grpcHelper.Send(x => x.PlaceOrderAsync(new Protos.PlaceOrderRequest())).ResponseAsync);
+
+            Assert.Equal(StatusCode.InvalidArgument, exception.StatusCode);
+
+            Assert.Contains(
+              exception.GetValidationErrors(),
+              e => e.PropertyName.EndsWith(errorPropertyName)
+            );
+        }
+    }
+}
