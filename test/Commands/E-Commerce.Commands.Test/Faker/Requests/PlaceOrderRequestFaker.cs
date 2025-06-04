60f210b55c82d79979cdd338a697815f169b6e46
saddik-maziad <saddikmaziad@gmail.com>
2025-06-04 15:12:34 +0300

    Add PalceOrderRequestFaker

---
+++ test/Commands/E-Commerce.Commands.Test/Faker/Requests/PlaceOrderRequestFaker.cs
@@ -0,0 +1,21 @@
+﻿using Bogus;
+using E_Commerce.Commands.Test.Protos;
+
+namespace Anis.TransactionsDateManagement.Commands.Test.Fakers.RequestFaker
+{
+    public class PlaceOrderRequestFaker : Faker<PlaceOrderRequest>
+    {
+        public PlaceOrderRequestFaker()
+        {
+            RuleFor(x => x.UserId, f => f.Random.Guid().ToString());
+            RuleFor(x => x.OrderId, f => f.Random.Guid().ToString());
+            RuleFor(x => x.Customer, f => new Customer
+            {
+                Id = f.Random.Guid().ToString(),
+                Email = f.Person.Email,
+                Name = f.Person.FirstName,
+            });
+            RuleFor(x => x.OrderItems, f => []);
+        }
+    }
+}
